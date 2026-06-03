717c7cc6a8aca0286ae8ab6f3c1d9680aad3cd41
sayalip13 <sayalipatil4162@gmail.com>
2026-06-03 18:10:11 +0530

    new project

---
+++ src/main/java/com/osv/heathcaresystem/HeathCareSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.osv.heathcaresystem;
+
+/**
+ *
+ * @author VELOX
+ */
+public class HeathCareSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
